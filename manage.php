d043a209d78a186fe73146d6c68eb4c7cff7d199
Raj Patel <riskyrick20@gmail.com>
2023-04-19 16:23:17 +0530

    bug fix

--- manage.php
+++ manage.php
@@ -3,20 +3,21 @@ session_start();
 include("connection.php");
 include("function.php"); //from function.php
 $user_data = check_login($con);
-if(isset($user_data["property_id"])){
-if (isset($_GET['dlt_id'])) {
-    $dlt_id = $_GET['dlt_id'];
-    $delete = mysqli_query($con, "DELETE FROM `property` WHERE `property_id`='$dlt_id'");
-    unset($_GET['dlt_id']);
-    header("Location: manage.php");
-}
+if (isset($user_data["property_id"])) {
+    if (isset($_GET['dlt_id'])) {
+        $dlt_id = $_GET['dlt_id'];
+        $delete = mysqli_query($con, "DELETE FROM `property` WHERE `property_id`='$dlt_id'");
+        unset($_GET['dlt_id']);
+        header("Location: manage.php");
+    }
 
-$query = "select property_id,property_name,city FROM property";
-$all_property = $con->query($query);
-$table = array();
-while ($row = mysqli_fetch_assoc($all_property)) {
-    $table[] = $row;
-}}
+    $query = "select property_id,property_name,city FROM property";
+    $all_property = $con->query($query);
+    $table = array();
+    while ($row = mysqli_fetch_assoc($all_property)) {
+        $table[] = $row;
+    }
+}
 ?>
 
 
@@ -31,47 +32,60 @@ while ($row = mysqli_fetch_assoc($all_property)) {
     <title>manage</title>
     <link rel="stylesheet" type="text/css" href="style.css" />
     <style>
-        .box{
-            display:flex; 
-            flex-direction:row; 
-            justify-content:center;
+        .box {
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
         }
     </style>
 </head>
 
 <body>
     <?php include("./shared/header.php") ?>
-    <?php if(isset($user_data["property_id"])){?>
-    <div><?php echo "<a href='upload.php'>New</a>"; ?></div>
-    <div class="box">
-        <table>
-            <tr>
-                <td><h4>Sr.No</h4></td>
-                <td><h4>Property Id</h4></td>
-                <td><h4>Property Name</h4></td>
-                <td><h4>City</h4></td>
-                <td><h4>Edit</h4></td>
-                <td><h4>Delete</h4></td>
-
-            </tr>
-            <?php $sr = 1;
-            foreach ($table as $row) {
-                $id = $row["property_id"]; ?>
+    <?php if (isset($user_data["user_id"])) { ?>
+        <div><?php echo "<a href='upload.php'>New</a>"; ?></div>
+        <div class="box">
+            <table>
                 <tr>
-                    <td><?php echo $sr++; ?></td>
-                    <td><?php echo $row["property_id"]; ?></td>
-                    <td><?php echo $row["property_name"]; ?></td>
-                    <td><?php echo $row["city"]; ?></td>
-                    <td><?php echo "<a href='index.php'>Edit</a>"; ?></td>
-                    <td><?php echo "<a href='manage.php?dlt_id=", urlencode($id), "'>Delete</a>"; ?></td>
+                    <td>
+                        <h4>Sr.No</h4>
+                    </td>
+                    <td>
+                        <h4>Property Id</h4>
+                    </td>
+                    <td>
+                        <h4>Property Name</h4>
+                    </td>
+                    <td>
+                        <h4>City</h4>
+                    </td>
+                    <td>
+                        <h4>Edit</h4>
+                    </td>
+                    <td>
+                        <h4>Delete</h4>
+                    </td>
 
                 </tr>
-            <?php }}else{
-                echo"Please Login";
-                }?>
-        </table>
-    </div>
-    <?php include("./shared/footer.php") ?>
+                <?php $sr = 1;
+                foreach ($table as $row) {
+                    $id = $row["property_id"]; ?>
+                    <tr>
+                        <td><?php echo $sr++; ?></td>
+                        <td><?php echo $row["property_id"]; ?></td>
+                        <td><?php echo $row["property_name"]; ?></td>
+                        <td><?php echo $row["city"]; ?></td>
+                        <td><?php echo "<a href='index.php'>Edit</a>"; ?></td>
+                        <td><?php echo "<a href='manage.php?dlt_id=", urlencode($id), "'>Delete</a>"; ?></td>
+
+                    </tr>
+            <?php }
+            } else {
+                echo "Please Login";
+            } ?>
+            </table>
+        </div>
+        <?php include("./shared/footer.php") ?>
 
 </body>
 
--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 
 LAMPT Project by
 
-1. [Imrraaj](github.com/imrraaj) 20BCE218
+1. [Imrraaj](https://github.com/imrraaj) 20BCE218
 2. [Vivek](github.com/imrraaj) 20BCE226
 3. [Purvajit](github.com/imrraaj) 20BCE238
 
